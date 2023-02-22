cefd05144c440b698da260d03e92a6a4e411a512
Drew Linsley <drewlinsley@gmail.com>
2023-02-21 21:32:35 -0500

    Fixed config.

--- app.py
+++ app.py
@@ -214,8 +214,9 @@ class ClipCaptionModel(nn.Module):
         super(ClipCaptionModel, self).__init__()
         self.prefix_length = prefix_length
         # self.gpt = GPT2LMHeadModel.from_pretrained('gpt2')
-        config = AutoConfig.from_pretrained(PRETRAINED)
-        self.gpt = AutoModelForCausalLM.from_config(config)  # .from_pretrained(PRETRAINED)
+        # config = AutoConfig.from_pretrained(PRETRAINED)
+        # self.gpt = AutoModelForCausalLM.from_config(config)  # .from_pretrained(PRETRAINED)
+        self.gpt = AutoModelForCausalLM.from_pretrained(PRETRAINED)
         if freeze:
             for param in self.gpt.parameters():
                 param.requires_grad = False
